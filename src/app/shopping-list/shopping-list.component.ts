70ed82574724c533ed98d2e84fdb73b809a8ec47
lwerner27 <lwerner27@gmail.com>
2018-12-21 11:50:13 -0600

    added an ingredient model for shared use through the app

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -5,7 +5,9 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
+
 export class ShoppingListComponent implements OnInit {
+  ingredients = [];
 
   constructor() { }
 
